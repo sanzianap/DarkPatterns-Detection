3ab821204b7d67c3f8907a018386686c3a6c1f95
sanzianap <prichicis@gmail.com>
2024-07-11 22:13:42 +0300

    Stable version with Reject, X and Custom button

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,2 +1,9 @@
-export const COOKIE_DIV_KEYWORDS = ['cookie', 'gdpr'];
-export const FALLBACKMECHANISM_ELEMENTS = ['id', 'class', 'aria-label'];
+export const WORDS = {
+    // COOKIE_DIV_KEYWORDS: ['cookie', 'gdpr', 'onetrust'],
+    COOKIE_DIV_KEYWORDS: ['cookie', 'gdpr'],
+    FALLBACKMECHANISM_ELEMENTS: ['id', 'class', 'aria-label'],
+    CLOSE_BUTTON: 'close',
+    REJECT_BUTTON_KEYWORDS: ['reject', 'decline', 'refuse'],
+    ALL: 'all',
+    CUSTOMISE_COOKIE_BUTTON: ['manage', 'custom', 'setting', 'preference', 'onetrust-pc-btn-handler']
+};
--- scripts/cookie_detection.js
+++ scripts/cookie_detection.js
@@ -1,5 +1,5 @@
 // THIS DOESN'T WORK FOR NOW; See .improvements/moduleType
-import { COOKIE_DIV_KEYWORDS, FALLBACKMECHANISM_ELEMENTS } from './constants.js';
+import { WORDS } from './constants.js';
 import _ from 'lodash';
 //import cors from 'cors';
 //import thesaurus from 'powerthesaurus-api';
@@ -42,6 +42,7 @@ const insertDomElementIfSuitable = (list, domElement) => {
     list.push(domElement);
     return true;
     // check if id>class>aria-label
+    // 
 }
 // const getApiSynonyms = () => {
 //     app.get('/synonyms/:word', async (req, res) => {
@@ -58,32 +59,86 @@ const insertDomElementIfSuitable = (list, domElement) => {
 // --------------------------------- Functions section end ------------------------------
 
 let allKeywords = [], divsRetreived = [];
-let x = "//div[contains(@id, 'cookie')]";
-let result = document.evaluate(x, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-console.log(result.snapshotLength);
 
-COOKIE_DIV_KEYWORDS.forEach(keyword => getAllCasesForKeyword(keyword, allKeywords));
+WORDS.COOKIE_DIV_KEYWORDS.forEach(keyword => getAllCasesForKeyword(keyword, allKeywords));
 // XPath expression to select divs with 'cookie' in their id
-let blackList = computeBlackList(allKeywords, FALLBACKMECHANISM_ELEMENTS);
-FALLBACKMECHANISM_ELEMENTS.forEach(domElement => {
+let blackList = computeBlackList(allKeywords, WORDS.FALLBACKMECHANISM_ELEMENTS);
+WORDS.FALLBACKMECHANISM_ELEMENTS.forEach(domElement => {
     allKeywords.forEach(keyword => {
-        const xpath = "//div[contains(@" + domElement + ", '" + keyword + "')" + blackList;
-        const result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        var xpath = "//div[contains(@" + domElement + ", '" + keyword + "')" + blackList;
+        let result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
         for (let i = 0; i < result.snapshotLength; i++) {
-            console.log("cvea");
-            let div = result.snapshotItem(i);
+            var div = result.snapshotItem(i);
             insertDomElementIfSuitable(divsRetreived, div);
         }
     });
 });
 
-console.log(divsRetreived);
-divsRetreived.forEach(div => console.log("width: " + div.clientWidth + " height: " + div.clientHeight));
-//divsRetreived[0].style.border = "2px solid red";
+divsRetreived.forEach(div => {
+    console.log("width: " + div.clientWidth + " height: " + div.clientHeight); div.style.border = "2px solid red";
+});
 
-//getApiSynonyms();
+let rejectbuttons = [];
+WORDS.REJECT_BUTTON_KEYWORDS.forEach(reject_button => {
+    WORDS.FALLBACKMECHANISM_ELEMENTS.forEach(domElement => {
+        var xpath = "//button[contains(@" + domElement + ", '" + reject_button + "')]";
+        let result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        for (let i = 0; i < result.snapshotLength; i++) {
+            var div = result.snapshotItem(i);
+            rejectbuttons.push(div);
+        }
+        console.log(xpath);
+        xpath = "//div[contains(@" + domElement + ", '" + reject_button + "')]";
+        result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        for (let i = 0; i < result.snapshotLength; i++) {
+            var div = result.snapshotItem(i);
+            rejectbuttons.push(div);
+        }
+        console.log(xpath);
+    });
+});
 
+// iterate thru all rejctbuttons and chose the right one
+console.log(rejectbuttons)
 
-//console.log(_.isString(":Sanziana:"));
-//console.log(COOKIE_DIV_KEYWORDS[0]);
-//getApiSynonyms();
+let closebuttons = [];
+WORDS.FALLBACKMECHANISM_ELEMENTS.forEach(domElement => {
+    var xpath = "//button[contains(@" + domElement + ", '" + WORDS.CLOSE_BUTTON + "')]";
+    let result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0; i < result.snapshotLength; i++) {
+        var div = result.snapshotItem(i);
+        closebuttons.push(div);
+    }
+    console.log(xpath);
+    xpath = "//div[contains(@" + domElement + ", '" + WORDS.CLOSE_BUTTON + "')]";
+    result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0; i < result.snapshotLength; i++) {
+        var div = result.snapshotItem(i);
+        closebuttons.push(div);
+    }
+    console.log(xpath);
+});
+
+console.log(closebuttons);
+
+let preferencebuttons = [];
+WORDS.CUSTOMISE_COOKIE_BUTTON.forEach(customise_button => {
+    WORDS.FALLBACKMECHANISM_ELEMENTS.forEach(domElement => {
+        var xpath = "//button[contains(@" + domElement + ", '" + customise_button + "')]";
+        let result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        for (let i = 0; i < result.snapshotLength; i++) {
+            var div = result.snapshotItem(i);
+            preferencebuttons.push(div);
+        }
+        console.log(xpath);
+        xpath = "//div[contains(@" + domElement + ", '" + customise_button + "')]";
+        result = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        for (let i = 0; i < result.snapshotLength; i++) {
+            var div = result.snapshotItem(i);
+            preferencebuttons.push(div);
+        }
+        console.log(xpath);
+    });
+});
+
+console.log(preferencebuttons);
