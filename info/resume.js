dda805a6ea732e64fe36753387baf7c46259ba5d
sanzianap <prichicis@gmail.com>
2024-07-20 17:39:35 +0300

    Version with resume.html

--- info/resume.js
+++ info/resume.js
@@ -1,14 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.get(['oldStyles'], (result) => {
-        const dataElement = document.getElementById("cookie_stats");
+        let dataElement = document.getElementById("cookie_stats");
         if (result.oldStyles.cookieDiv.id) {
             dataElement.textContent = "We've found the cookie div!";
         } else {
             dataElement.textContent = "Cookie div can't be located";
         }
+        dataElement = document.getElementById("rejectButton_stats");
+        if (result.oldStyles.rejectButton.id) {
+            dataElement.textContent = "We've found the reject button!";
+        } else {
+            dataElement.textContent = "Reject button can't be located";
+        }
+        dataElement = document.getElementById("closeButton_stats");
+        if (result.oldStyles.closeButton.id) {
+            dataElement.textContent = "We've found the close button!";
+        } else {
+            dataElement.textContent = "Close button can't be located";
+        }
+        dataElement = document.getElementById("buttonsToDisplay");
+        if (!result.oldStyles.preferenceButton.id) {
+            dataElement.style.visibility = "hidden";
+        }
     });
 });
 
-chrome.runtime.sendMessage({ action: 'hello' }, (response) => {
-    console.log("Response from content script:", response);
-});
+function clickPreferencesButton() {
+    chrome.runtime.sendMessage({ action: 'clickPreferences' }, (response) => {
+        console.log("Response from content script:", response);
+    });
+}
